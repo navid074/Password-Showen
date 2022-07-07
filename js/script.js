8c3af592a4b30ebc9c121b11226c786a5045cd52
navid074 <13navid074@gmail.com>
2022-07-07 11:30:27 +0430

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+﻿ // Functions for select elems with id & class
+ let $ = document;
+ function _id(id_name) {
+   return $.getElementById(id_name);
+ }
+
+ function _class(class_name) {
+   return $.getElementsByClassName(class_name);
+ }
+
+ // Select Elems
+ let togglePassword = _class("toggle-password");
+ let passwordField = _id("password-field");
+ let toggleIcon = document.querySelector('.fa-eye');
+
+ // Fire click event on eye icon
+ toggleIcon.addEventListener("click", function () {
+   if (passwordField.type == "text") {
+     passwordField.type = "password";
+     toggleIcon.classList.remove("active");
+   } else {
+     passwordField.type = "text";
+     toggleIcon.classList.add("active");
+   }
+ })
